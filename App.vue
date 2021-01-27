3e96eadbbe6e23a11c201dc292b615be313ea05a
cho_daejoon <cho_daejoon@nuvo.co.jp>
2021-01-27 09:52:34 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <VueHeader />
+    <div id="content" class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+  import VueHeader from './components/layout/Header.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      VueHeader
+    }
+  }
+</script>
